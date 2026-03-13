807b8bd4e190dc8ed3146db054b18e666ccac897
Will Jarrell <wjarrell@crossings.church>
2026-03-13 18:18:13 +0000

    Fix font publicPath for webpack 5, fix sass-loader API warning, bump version to 1.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 
-
 module.exports = {
   devtool: 'source-map',
   mode: 'development',
-  // entry: ['./js/script.js','./js/gif.js','./js/chart-smoothie.js','./js/demodata.js'],
   entry: {
     app: ['whatwg-fetch', './js/app.js'],
     about: ['./js/about.js'],
@@ -22,7 +20,7 @@ module.exports = {
       jQuery: 'jquery',
     }),
     new webpack.DefinePlugin({
-      VERSION: JSON.stringify(require("./package.json").version)
+      VERSION: JSON.stringify(require('./package.json').version),
     }),
   ],
   module: {
@@ -33,38 +31,32 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [{
-          loader: 'style-loader',
-        }, {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
           },
-        }, {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true, api: 'modern' },
           },
-        }],
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/',
-            publicPath: 'static/fonts/',
-          },
-        }],
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+          publicPath: '/static/',
+        },
       },
       {
-        test: /.jsx?$/,
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        query: {
+        options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
-          // presets: ['env', 'react']
         },
       },
     ],
